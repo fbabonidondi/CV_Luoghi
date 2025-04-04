370983ff68bd7a2665092d98c579e05809501011
fbaboni <francesco.baboni@dondisalotti.it>
2025-04-04 16:39:27 +0200

    [ADD] responsive design adjustments for StatistichePage layout

--- src/components/LuogoDetails.vue
+++ src/components/LuogoDetails.vue
@@ -1,81 +1,302 @@
 <template>
-  <!-- Remove the v-dialog wrapper since it's already in the parent -->
   <div>
-    <v-card-text class="pa-6">
-      <v-row>
-        <v-col cols="12" md="6">
-          <v-card variant="outlined" class="pa-4 mb-4">
-            <v-card-title class="text-h6">Informazioni</v-card-title>
-            
-            <v-list density="comfortable">
-              <v-list-item>
-                <template v-slot:prepend>
-                  <v-icon icon="mdi-calendar"></v-icon>
-                </template>
-                <v-list-item-subtitle>Data visita</v-list-item-subtitle>
-                <v-list-item-title>{{ formatDate(luogo.data_visita) }}</v-list-item-title>
-              </v-list-item>
+    <!-- Header con colore basato sulla categoria -->
+    <v-card-item 
+      :color="getHeaderColor(luogo.categoria)" 
+      class="text-white"
+    >
+      <v-card-title class="text-h5 d-flex align-center">
+        <v-icon 
+          :icon="getCategoriaIcon(luogo.categoria)" 
+          class="mr-2" 
+          size="large"
+        ></v-icon>
+        {{ luogo.nome }}
+      </v-card-title>
+      <v-card-subtitle class="text-white-opacity mt-1">
+        {{ formatCategoria(luogo.categoria) }} • {{ formatDate(luogo.data_visita) }}
+      </v-card-subtitle>
+    </v-card-item>
 
-              <v-list-item>
-                <template v-slot:prepend>
-                  <v-icon icon="mdi-map-marker"></v-icon>
-                </template>
-                <v-list-item-subtitle>Indirizzo</v-list-item-subtitle>
-                <v-list-item-title>{{ luogo.indirizzo }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-card>
+    <!-- Tabs per navigare tra le sezioni -->
+    <v-tabs
+      v-model="activeTab"
+      :bg-color="getCategoriaColor(luogo.categoria)"
+      align-tabs="center"
+      grow
+      density="compact"
+    >
+      <v-tab value="info">
+        <v-icon start>mdi-information-outline</v-icon>
+        Info
+      </v-tab>
+      <v-tab value="details">
+        <v-icon start>mdi-card-text-outline</v-icon>
+        Dettagli
+      </v-tab>
+      <v-tab value="related">
+        <v-icon start>mdi-link-variant</v-icon>
+        Correlati
+      </v-tab>
+      <v-tab value="map">
+        <v-icon start>mdi-map-outline</v-icon>
+        Mappa
+      </v-tab>
+    </v-tabs>
 
-          <v-card variant="outlined" class="pa-4">
-            <v-card-title class="text-h6">Descrizione</v-card-title>
-            <v-card-text>{{ luogo.descrizione }}</v-card-text>
-          </v-card>
-        </v-col>
+    <!-- Contenuto delle tabs -->
+    <v-window v-model="activeTab">
+      <!-- Tab Informazioni -->
+      <v-window-item value="info">
+        <v-container class="pa-3">
+          <v-row>
+            <v-col cols="12">
+              <div class="d-flex align-center mb-2">
+                <v-avatar :color="getHeaderColor(luogo.categoria)" size="32" class="mr-3">
+                  <v-icon color="white" :icon="getCategoriaIcon(luogo.categoria)"></v-icon>
+                </v-avatar>
+                <div>
+                  <div class="text-body-1 font-weight-bold text-dark">{{ luogo.nome }}</div>
+                  <div class="text-caption text-grey-darken-3">{{ formatCategoria(luogo.categoria) }}</div>
+                </div>
+              </div>
 
-        <v-col cols="12" md="6">
-          <v-card variant="outlined" class="pa-4 mb-4">
-            <v-card-title class="text-h6">Dettagli specifici</v-card-title>
-            <!-- Campi specifici per categoria (stesso contenuto) -->
-          </v-card>
+              <v-divider class="mb-2"></v-divider>
 
-          <v-card variant="outlined" class="pa-4">
-            <v-card-title class="text-h6">Luoghi correlati</v-card-title>
-            <v-card-text>
-              <v-chip-group>
-                <v-chip
+              <v-list density="compact" class="pa-0 bg-transparent">
+                <v-list-item class="px-0">
+                  <template v-slot:prepend>
+                    <v-icon icon="mdi-calendar" color="grey-darken-1"></v-icon>
+                  </template>
+                  <v-list-item-title class="text-dark">Data visita</v-list-item-title>
+                  <v-list-item-subtitle class="text-grey-darken-3">{{ formatDate(luogo.data_visita) }}</v-list-item-subtitle>
+                </v-list-item>
+
+                <v-list-item class="px-0">
+                  <template v-slot:prepend>
+                    <v-icon icon="mdi-map-marker" color="grey-darken-1"></v-icon>
+                  </template>
+                  <v-list-item-title class="text-dark">Indirizzo</v-list-item-title>
+                  <v-list-item-subtitle class="text-grey-darken-3">{{ luogo.indirizzo }}</v-list-item-subtitle>
+                </v-list-item>
+              </v-list>
+
+              <v-divider class="my-2"></v-divider>
+
+              <div>
+                <div class="text-subtitle-1 font-weight-bold mb-1 text-dark">Descrizione</div>
+                <v-card 
+                  variant="outlined" 
+                  class="pa-2" 
+                  :color="`${getCategoriaColor(luogo.categoria)}-lighten-5`"
+                >
+                  <div class="text-dark">{{ luogo.descrizione }}</div>
+                </v-card>
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-window-item>
+
+      <!-- Tab Dettagli -->
+      <v-window-item value="details">
+        <v-container class="pa-3">
+          <v-row>
+            <v-col cols="12">
+              <div class="text-subtitle-1 font-weight-bold mb-2 text-dark">Dettagli specifici</div>
+
+              <!-- Commento sempre presente per tutti i luoghi -->
+              <v-card
+                variant="outlined"
+                class="mb-2 pa-2"
+                :color="`${getCategoriaColor(luogo.categoria)}-lighten-5`"
+              >
+                <div class="text-subtitle-2 font-weight-bold mb-1 text-dark">Commento</div>
+                <div class="text-dark">{{ luogo.commento || 'Nessun commento disponibile' }}</div>
+              </v-card>
+              
+              <!-- Campi specifici per categoria -->
+              <template v-if="luogo.categoria === 'ristorante' && luogo.lista_cibi">
+                <v-card
+                  variant="outlined"
+                  class="mb-2 pa-2"
+                  :color="`${getCategoriaColor(luogo.categoria)}-lighten-5`"
+                >
+                  <div class="text-subtitle-2 font-weight-bold mb-1 text-dark">Lista cibi</div>
+                  <div class="d-flex flex-wrap">
+                    <v-chip
+                      v-for="(cibo, index) in luogo.lista_cibi"
+                      :key="index"
+                      size="small"
+                      class="ma-1"
+                      :color="getHeaderColor(luogo.categoria)"
+                      label
+                    >
+                      {{ cibo }}
+                    </v-chip>
+                  </div>
+                </v-card>
+              </template>
+              
+              <template v-if="luogo.categoria === 'rifugio' && luogo.altitudine">
+                <v-card
+                  variant="outlined"
+                  class="mb-2 pa-2"
+                  :color="`${getCategoriaColor(luogo.categoria)}-lighten-5`"
+                >
+                  <div class="text-subtitle-2 font-weight-bold mb-1 text-dark">Altitudine</div>
+                  <div class="d-flex align-center text-dark">
+                    <v-icon icon="mdi-elevation-rise" class="mr-2" :color="getHeaderColor(luogo.categoria)"></v-icon>
+                    <span>{{ luogo.altitudine }} metri s.l.m.</span>
+                  </div>
+                </v-card>
+              </template>
+              
+              <template v-if="luogo.categoria === 'lago' && luogo.superficie">
+                <v-card
+                  variant="outlined"
+                  class="mb-2 pa-2"
+                  :color="`${getCategoriaColor(luogo.categoria)}-lighten-5`"
+                >
+                  <div class="text-subtitle-2 font-weight-bold mb-1 text-dark">Superficie</div>
+                  <div class="d-flex align-center text-dark">
+                    <v-icon icon="mdi-waves" class="mr-2" :color="getHeaderColor(luogo.categoria)"></v-icon>
+                    <span>{{ luogo.superficie }} km²</span>
+                  </div>
+                </v-card>
+              </template>
+              
+              <template v-if="luogo.categoria === 'parco_naturale' && luogo.estensione">
+                <v-card
+                  variant="outlined"
+                  class="mb-2 pa-2"
+                  :color="`${getCategoriaColor(luogo.categoria)}-lighten-5`"
+                >
+                  <div class="text-subtitle-2 font-weight-bold mb-1 text-dark">Estensione</div>
+                  <div class="d-flex align-center text-dark">
+                    <v-icon icon="mdi-ruler-square" class="mr-2" :color="getHeaderColor(luogo.categoria)"></v-icon>
+                    <span>{{ luogo.estensione }} ettari</span>
+                  </div>
+                </v-card>
+              </template>
+              
+              <!-- Fallback per altre categorie o campi mancanti -->
+              <v-alert v-if="!hasSpecificDetails" type="info" variant="tonal" density="compact" class="mt-2">
+                Nessun dettaglio specifico disponibile per questa categoria
+              </v-alert>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-window-item>
+
+      <!-- Tab Luoghi correlati -->
+      <v-window-item value="related">
+        <v-container class="pa-3">
+          <v-row>
+            <v-col cols="12">
+              <div class="text-subtitle-1 font-weight-bold mb-2 text-dark">Luoghi correlati</div>
+              
+              <template v-if="relatedLuoghi.length > 0">
+                <v-card
                   v-for="related in relatedLuoghi"
                   :key="related.id"
                   variant="outlined"
+                  class="mb-2"
+                  :color="`${getCategoriaColor(related.categoria)}-lighten-5`"
                   @click="selectRelated(related)"
                 >
-                  {{ related.nome }}
-                </v-chip>
-              </v-chip-group>
-              <v-alert v-if="relatedLuoghi.length === 0" type="info" variant="tonal">
+                  <v-card-item class="pa-2">
+                    <template v-slot:prepend>
+                      <v-avatar :color="getHeaderColor(related.categoria)" size="32" class="mr-2">
+                        <v-icon color="white" :icon="getCategoriaIcon(related.categoria)"></v-icon>
+                      </v-avatar>
+                    </template>
+                    <v-card-title class="text-body-1 text-dark">{{ related.nome }}</v-card-title>
+                    <v-card-subtitle class="text-caption text-grey-darken-3">{{ formatCategoria(related.categoria) }}</v-card-subtitle>
+                  </v-card-item>
+                </v-card>
+              </template>
+              
+              <v-alert v-else type="info" variant="tonal" density="compact" class="mt-2">
+                <template v-slot:prepend>
+                  <v-icon icon="mdi-information-outline"></v-icon>
+                </template>
                 Nessun luogo correlato trovato
               </v-alert>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-card-text>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-window-item>
 
-    <v-card-actions class="pa-4">
-      <v-spacer></v-spacer>
-      <v-btn variant="tonal" @click="closeDetails">Chiudi</v-btn>
+      <!-- Tab Mappa -->
+      <v-window-item value="map">
+        <v-container class="pa-3">
+          <v-row>
+            <v-col cols="12">
+              <div class="text-subtitle-1 font-weight-bold mb-2 text-dark">Posizione sulla mappa</div>
+              
+              <v-card 
+                class="map-placeholder d-flex align-center justify-center" 
+                height="250"
+                :color="`${getCategoriaColor(luogo.categoria)}-lighten-5`"
+                variant="outlined"
+              >
+                <div class="text-center">
+                  <v-icon icon="mdi-map-marker" size="large" :color="getHeaderColor(luogo.categoria)" class="mb-2"></v-icon>
+                  <div class="text-body-1 text-dark">{{ luogo.indirizzo }}</div>
+                  <div class="text-caption text-grey-darken-3">
+                    (Qui verrebbe visualizzata la mappa per questo luogo)
+                  </div>
+                </div>
+              </v-card>
+              
+              <v-btn 
+                :color="getHeaderColor(luogo.categoria)" 
+                variant="tonal" 
+                block 
+                class="mt-2"
+                prepend-icon="mdi-directions"
+                size="small"
+              >
+                Ottieni indicazioni
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-window-item>
+    </v-window>
+
+    <!-- Footer con azioni -->
+    <v-card-actions class="pa-2">
       <v-btn 
-        color="primary" 
-        variant="tonal"
-        class="mx-1"
-        size="large"
+        variant="text" 
+        @click="closeDetails"
+        prepend-icon="mdi-close"
+        size="small"
       >
-        <v-icon start>mdi-map</v-icon>
-        Vedi sulla mappa
+        Chiudi
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn 
+        :color="getHeaderColor(luogo.categoria)" 
+        variant="tonal"
+        prepend-icon="mdi-share-variant"
+        class="mr-1"
+        size="small"
+      >
+        Condividi
+      </v-btn>
+      <v-btn 
+        :color="getHeaderColor(luogo.categoria)" 
+        prepend-icon="mdi-heart-outline"
+        size="small"
+      >
+        Salva
       </v-btn>
     </v-card-actions>
   </div>
 </template>
-  
+
 <script>
 export default {
   name: 'LuogoDetails',
@@ -89,17 +310,33 @@ export default {
       default: () => []
     }
   },
+  data() {
+    return {
+      activeTab: 'info',
+    }
+  },
   computed: {
-    // Trova luoghi correlati (stessa categoria o data di visita simile)
     relatedLuoghi() {
-      // Se non ci sono altri luoghi, restituisci un array vuoto
       if (!this.luoghi || this.luoghi.length <= 1) return [];
       
       return this.luoghi
         .filter(l => l.id !== this.luogo.id)
         .filter(l => l.categoria === this.luogo.categoria || 
                     this.isSimilarDate(l.data_visita, this.luogo.data_visita))
-        .slice(0, 3); // Limitiamo a 3 luoghi correlati
+        .slice(0, 3);
+    },
+    
+    hasSpecificDetails() {
+      const categoria = this.luogo.categoria;
+      
+      if (!categoria) return false;
+      
+      if (categoria === 'ristorante' && this.luogo.lista_cibi && this.luogo.lista_cibi.length > 0) return true;
+      if (categoria === 'rifugio' && this.luogo.altitudine) return true;
+      if (categoria === 'lago' && this.luogo.superficie) return true;
+      if (categoria === 'parco_naturale' && this.luogo.estensione) return true;
+      
+      return false;
     }
   },
   methods: {
@@ -161,20 +398,18 @@ export default {
       if (!categoria) return 'grey';
       
       const colorMap = {
-        'ristorante': 'red-lighten-1',
-        'chiesa': 'blue-lighten-2',
-        'museo': 'amber-lighten-2',
-        'rifugio': 'green-lighten-1',
-        'lago': 'blue-lighten-1',
-        'parco_naturale': 'green-lighten-1'
+        'ristorante': 'red',
+        'chiesa': 'blue',
+        'museo': 'amber',
+        'rifugio': 'green',
+        'lago': 'blue',
+        'parco_naturale': 'green'
       };
       return colorMap[categoria] || 'grey';
     },
     isSimilarDate(date1, date2) {
-      // Se una delle date non esiste, ritorna false
       if (!date1 || !date2) return false;
       
-      // Controlla se le date sono entro 30 giorni l'una dall'altra
       const d1 = new Date(date1);
       const d2 = new Date(date2);
       const diffTime = Math.abs(d2 - d1);
@@ -184,45 +419,38 @@ export default {
   }
 }
 </script>
-  
+
 <style scoped>
-.luogo-details-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-  padding: 20px;
+.text-white-opacity {
+  opacity: 0.9;
 }
 
-.luogo-details-modal {
-  width: 100%;
-  max-width: 800px;
-  max-height: 90vh;
-  overflow-y: auto;
-  animation: fadeIn 0.3s;
+.v-card-title {
+  word-break: break-word;
+  line-height: 1.3;
 }
 
-.detail-section h4 {
-  font-size: 0.85rem;
-  margin-bottom: 0.5rem;
-  letter-spacing: 0.5px;
+.v-card-subtitle {
+  line-height: 1.2;
 }
 
-.detail-section {
-  background-color: #ffffff;
-  border-radius: 0.25rem;
-  padding: 0.75rem;
-  border-left: 3px solid #e9ecef;
+.v-card-item {
+  padding: 12px !important;
 }
 
-@keyframes fadeIn {
-  from { opacity: 0; transform: translateY(-20px); }
-  to { opacity: 1; transform: translateY(0); }
+.v-avatar {
+  --v-avatar-height: 32px !important;
+}
+
+.map-placeholder {
+  border: 1px dashed rgba(0,0,0,0.1);
+}
+
+.text-dark {
+  color: rgba(0, 0, 0, 0.87);
+}
+
+.text-grey-darken-3 {
+  color: rgba(0, 0, 0, 0.6);
 }
 </style>
